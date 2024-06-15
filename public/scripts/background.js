ad4717d50fb71eef9fbe28b04fc42a0384ade4dd
shamin <shamin.asfaq@gmail.com>
2024-06-15 03:38:23 +0200

    - A working solution.

--- public/scripts/background.js
+++ public/scripts/background.js
@@ -1,1 +1,28 @@
-console.log('Service Worker Here.')
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.action.setBadgeText({
+        text: "",
+    });
+
+    chrome.storage.local.remove('state');
+});
+
+chrome.action.onClicked.addListener(async (tab) => {
+    // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
+    const prevState = await chrome.action.getBadgeText({tabId: tab.id});
+    // Next state will always be the opposite
+    const nextState = prevState === 'ON' ? '' : 'ON'
+
+    // Set the action badge to the next state
+    await chrome.action.setBadgeText({
+        tabId: tab.id,
+        text: nextState,
+    });
+
+    if (nextState === 'ON') {
+        await chrome.action.setBadgeBackgroundColor({color: "#FF0000"});
+    }
+
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, { nextState });
+    });
+});
